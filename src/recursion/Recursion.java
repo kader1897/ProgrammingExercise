11661a2625053e5ad8707cda2caf0a899a21be3e
kader <noreply>
2018-12-30 01:35:53 +0300

    String permutation and combination

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -1,13 +1,17 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Recursion {
 
-    public static void stringPermutation(String s)
+    private static void stringPermutation(String s)
     {
         stringPermutation("", s);
     }
 
-    public static void stringPermutation(String prefix, String s)
+    private static void stringPermutation(String prefix, String s)
     {
         if(s.length() == 1)
             System.out.println(prefix + s);
@@ -21,18 +25,35 @@ public class Recursion {
         }
     }
 
-    public static void stringCombination(String s)
+    private static void stringCombination(String s)
     {
-
+        ArrayList<Character> chars = new ArrayList<>();
+        HashMap<Character, Boolean> map = new HashMap<>();
+        for (Character c: s.toCharArray()) {
+            if(!map.containsKey(c))
+            {
+                chars.add(c);
+                map.put(c,true);
+            }
+        }
+        for(int i = 1; i <= chars.size(); i++)
+            stringCombination(chars, "", 0, i);
     }
 
-    public static void stringCombination()
+    private static void stringCombination(List<Character> chars, String prefix, int beginIndex, int length)
     {
-        
+        if(length == 0) {
+            System.out.println(prefix);
+            return;
+        }
+        for(int j = beginIndex; j + length <= chars.size(); j++) {
+            stringCombination(chars, prefix + chars.get(j), j + 1, length - 1);
+        }
     }
 
     public static void main(String[] args)
     {
         stringPermutation("kader");
+        stringCombination("wxyz");
     }
 }
