9b7f083793684533cda75180276fad837285d998
kaderb <kaderbelli@yahoo.com>
2019-01-25 17:21:55 +0300

    Trie Tree implementation

---
+++ src/main/java/trees/TrieTree.java
@@ -0,0 +1,82 @@
+package trees;
+
+import java.util.Scanner;
+
+/**
+ * Created by kader.belli on 25.01.2019.
+ */
+public class TrieTree{
+    
+    public static void main(String args[] ) throws Exception {
+        
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int q = s.nextInt();
+    
+        TrieNode root = new TrieNode();
+        int i, j;
+        for(i = 0; i < n; i++)
+        {
+            TrieNode current = root;
+            char[] arr = s.next().trim().toCharArray();
+            int value = s.nextInt();
+            for(j = 0; j < arr.length - 1; j++)
+            {
+                char c = arr[j];
+                int index = charToIndex(c);
+                if(current.children[index] == null)
+                {
+                    TrieNode node = new TrieNode(c, null);
+                    current.children[index] = node;
+                    current = node;
+                }
+                else
+                {
+                    current = current.children[index];
+                }
+            }
+            
+            char c = arr[j];
+            int index = charToIndex(c);
+            TrieNode node = current.children[index];
+            if(node == null)
+            {
+                current.children[index] = new TrieNode(c, value);
+            }
+            else if(node.weight < value)
+            {
+                node.weight = value;
+            }
+        }
+        System.out.println();
+        
+    }
+    
+    private static int charToIndex(char c)
+    {
+        return c - 'a';
+    }
+}
+
+class TrieNode{
+    
+    final int numLetters = 'z' - 'a' + 1;
+    Character value;
+    Integer weight;
+    TrieNode[] children;
+    
+    TrieNode(Character value, Integer weight)
+    {
+        this.value = value;
+        this.weight = weight;
+        this.children = new TrieNode[numLetters];
+    }
+    
+    TrieNode()
+    {
+        this.value = null;
+        this.weight = null;
+        this.children = new TrieNode[numLetters];
+    }
+}
